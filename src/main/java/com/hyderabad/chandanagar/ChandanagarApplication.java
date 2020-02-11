90ca8594da0e77531a2110fd153f54cd9d6e3f87
parepallimallika <57309357+parepallimallika@users.noreply.github.com>
2020-02-11 22:05:34 +0530

    Update ChandanagarApplication.java

--- src/main/java/com/hyderabad/chandanagar/ChandanagarApplication.java
+++ src/main/java/com/hyderabad/chandanagar/ChandanagarApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChandanagarApplication {
 
-	//mallika
+	//mallika111
 	public static void main(String[] args) {
 		SpringApplication.run(ChandanagarApplication.class, args);
 	}

2df590655ad2ef1ccc7b7a5708055fd07a1d3edc
parepallimallika <mallika129.parepalli@gmail.com>
2020-02-11 21:57:13 +0530

    mallika

---
+++ src/main/java/com/hyderabad/chandanagar/ChandanagarApplication.java
@@ -0,0 +1,13 @@
+package com.hyderabad.chandanagar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChandanagarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChandanagarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Hyderabad/chandanagar/ChandanagarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hyderabad.chandanagar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChandanagarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
